2f83d66000688cea7aea803368fb2aefaac33df6
siddharthan21 <102781188+siddharthan21@users.noreply.github.com>
2024-06-27 19:50:59 +0530

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID:"767653808221-fm2k94r1g2pvvnu4u0pjgsd2sqchro2l.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "https://main-pfik.onrender.com/auth/google/callback",
+    callbackURL: "http://localhost:3000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
